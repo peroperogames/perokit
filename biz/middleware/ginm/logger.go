9a82f6315137fe75e9cfde96e8db30d354e3087a
codefish <codefish@peropero.net>
2021-07-22 16:18:55 +0800

    feat: 更新gin日志中间件

--- biz/middleware/ginm/logger.go
+++ biz/middleware/ginm/logger.go
@@ -4,23 +4,36 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/peroperogames/perokit/log"
+	"net/http"
 	"time"
 )
 
-// LoggerMiddleWare 日志拦截中间件
-func LoggerMiddleWare(c *gin.Context) {
-	start := time.Now()
-	c.Next()
-	end := time.Now()
-	latency := end.Sub(start)
-	path := c.Request.URL.Path
-	clientIP := c.ClientIP()
-	method := c.Request.Method
-	statusCode := c.Writer.Status()
-	log.Info(fmt.Sprintf("|STATUS: %d	|Latency: %v	|Client ip: %s	|method: %s	|path: %s	",
-		statusCode,
-		latency,
-		clientIP,
-		method,
-		path))
+// LoggerMiddle 日志拦截中间件
+func LoggerMiddle() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Start timer
+		start := time.Now()
+
+		// Process request
+		c.Next()
+		end := time.Now()
+		latency := end.Sub(start)
+		path := c.Request.RequestURI
+		clientIP := c.GetHeader("X-Forwarded-For")
+		if len(clientIP) == 0 {
+			clientIP = c.ClientIP()
+		}
+		method := c.Request.Method
+		statusCode := c.Writer.Status()
+		if method != http.MethodHead {
+			log.Info(fmt.Sprintf("|STATUS: %d	|Latency: %v	|Client ip: %s	|method: %s	|path: %s	",
+				statusCode,
+				latency,
+				clientIP,
+				method,
+				path))
+		}
+
+	}
+
 }
